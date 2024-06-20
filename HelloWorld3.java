f11fb7afacb8e1bd3320f1379136c94455240e71
Robert Minea <robert.leu2009@yahoo.com>
2024-06-20 21:14:37 +0300

    Add small change that we are proud of for HelloWorld3

--- HelloWorld3.java
+++ HelloWorld3.java
@@ -1,2 +1,6 @@
 package PACKAGE_NAME;public class HelloWorld3 {
+
+    public static void main(String[] args) {
+        System.out.println("Aceastea este o mica versiune a aplicatiei mele dar mult mai tare");
+    }
 }
