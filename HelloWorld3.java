8b45d82ffe6530b1013ad46bd10a72f56970f3dd
Robert Minea <robert.leu2009@yahoo.com>
2024-06-20 20:58:48 +0300

    Fix HelloWorld3

--- HelloWorld3.java
+++ HelloWorld3.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;public class HelloWorld3 {
+public class HelloWorld3 {
 
     public static void main(String[] args) {
         System.out.println("Aceastea este o mica versiune a aplicatiei mele dar mult mai tare");
